ffcd7b0e1a80b7f2e4bf4ce72f2ac19d3aa94777
gilbrater <gilbrater1@gmail.com>
2018-04-03 10:53:03 +0100

    Test setup for receiving notificatio

--- src/test/java/com/fortumo/gateway/GatewayApplicationTests.java
+++ src/test/java/com/fortumo/gateway/GatewayApplicationTests.java
@@ -1,16 +1,39 @@
 package com.fortumo.gateway;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GatewayApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
+    @LocalServerPort
+    private int port;
+
+    private String base;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new StringBuilder("http://localhost:").append(port).append("/api/v1/sms").toString();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = restTemplate.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(), equalTo("OK"));
+    }
 }
